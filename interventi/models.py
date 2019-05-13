222e233ded0c0eb02067b8c6dbfc7a5cfca281c8
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2019-05-13 15:18:53 +0200

    adding Fornitore and PuntoVendita models

--- interventi/models.py
+++ interventi/models.py
@@ -1,3 +1,47 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Fornitore(models.Model):
+    """
+    Modello contenente i dati di ogni fornitore
+    """
+    nome = models.CharField(max_length=250)
+    tipo_servizio_offerto = models.CharField(max_length=200)
+    indirizzo = models.CharField(blank=True, max_length=250)
+    citta = models.CharField(blank=True, max_length=200)
+    referente = models.CharField(blank=True, max_length=200)
+    email = models.EmailField(blank=True, max_length=200)
+    telefono = models.CharField(blank=True, max_length=200)
+    annotazioni = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Fornitori'
+
+    def __str__(self):
+        return f'{self.tipo_servizio_offerto} - {self.nome}'
+
+
+class PuntoVendita(models.Model):
+    """
+    Modello contenente i dati di ogni punto vendita
+    """
+    nome = models.CharField(max_length=250)
+    indirizzo = models.CharField(blank=True, max_length=250)
+    citta = models.CharField(blank=True, max_length=200)
+    responsabile = models.CharField(blank=True, max_length=200)
+    email = models.EmailField(blank=True, max_length=200)
+    telefono = models.CharField(blank=True, max_length=200)
+    annotazioni = models.TextField(blank=True)
+    fornitori_preferiti = models.ManyToManyField(
+        Fornitore, blank=True, related_name='punti_vendita'
+    )
+    utenti_preferiti = models.ManyToManyField(
+        User, blank=True, related_name='punti_vendita'
+    )
+
+    class Meta:
+        verbose_name_plural = 'PuntiVendita'
+
+    def __str__(self):
+        return self.nome
