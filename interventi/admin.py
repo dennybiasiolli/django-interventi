d9f060cf4d73f6a7199cead060032a07b56a6e22
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2019-05-31 11:01:36 +0200

    models(Intervento): adding urgente boolean field

--- interventi/admin.py
+++ interventi/admin.py
@@ -39,14 +39,15 @@ class InterventoAdmin(admin.ModelAdmin):
         InterventoAllegatoInline,
     )
     list_display = (
-        'titolo', 'punto_vendita', 'segnalatore',
+        'titolo', 'urgente', 'punto_vendita', 'segnalatore',
         'data_inserimento', 'data_ultima_modifica',
         'stato_cliente', 'stato_interno',
     )
     ordering = (
-        '-data_inserimento',
+        '-urgente', '-data_inserimento',
     )
     list_filter = (
+        'urgente',
         ('punto_vendita', admin.RelatedOnlyFieldListFilter),
         ('stato_cliente', admin.RelatedOnlyFieldListFilter),
         ('stato_interno', admin.RelatedOnlyFieldListFilter),
