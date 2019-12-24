329974fb06243757c8df15a05f3f35f8fc1884dd
Denny Biasiolli <denny.biasiolli@gmail.com>
2019-12-24 16:05:30 +0100

    disabling form in browsable api

--- mysite/settings_prod.py
+++ mysite/settings_prod.py
@@ -2,3 +2,8 @@ from .settings_base import *
 
 
 DEBUG = False
+
+REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = (
+    'rest_framework.renderers.JSONRenderer',
+    'utils.api.BrowsableAPIRendererWithoutForms',
+)
