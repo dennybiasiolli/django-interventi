b119da94847048ee08c3c7f4872e79b7fff360d4
Denny Biasiolli <denny.biasiolli@gmail.com>
2020-04-21 12:24:41 +0200

    website: adding swagger urls and configuration

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
-from rest_framework import routers
+from django.urls import include, path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions, routers
 from rest_framework.authtoken import views
 from rest_framework_simplejwt.views import (TokenRefreshView, TokenVerifyView)
 
@@ -29,6 +31,18 @@ ROUTER = routers.DefaultRouter()
 ROUTER.registry.extend(interventi_router.registry)
 ROUTER.register(r'user-info', UserInfoViewSet)
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title='Interventi API',
+        default_version='v1',
+        description='Interventi API documentation',
+        terms_of_service='https://www.google.com/policies/terms/',
+        contact=openapi.Contact(email='info@dennybiasiolli.com'),
+        license=openapi.License(name='BSD License'),
+    ),
+    public=False,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -37,7 +51,13 @@ urlpatterns = [
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('', include(ROUTER.urls)),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$',
+            schema_view.without_ui(cache_timeout=0),
+            name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
+            name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
+            name='schema-redoc'),    path('', include(ROUTER.urls)),
 ]
 
 if settings.DEBUG:
